c1554202f6f81784e48212401edf7d62425a3e67
ybenamour <yassinebenamour@gmail.com>
2022-11-22 19:54:13 +0100

    lab4

---
+++ SQL4.sql
@@ -0,0 +1,47 @@
+use sakila;
+
+-- Question 1 : 
+
+select rating from film;
+
+-- Question 2 : 
+
+select release_year from film;
+
+-- Question 3 :
+
+select title from film
+where title like '%ARMAGEDDON%';
+
+-- Question 4 :
+select title from film
+where title like '%APOLLO%';
+
+-- Question 5 : 
+
+select title from film
+where title like '%APOLLO'; 
+
+-- Question 6 : 
+select * from film
+where title like '%DATE%';
+
+-- Question 7 : 
+
+
+
+-- Question 8 : 
+
+-- Question 9 : 
+
+-- Question 10 : 
+
+
+
+
+
+
+
+
+
+
